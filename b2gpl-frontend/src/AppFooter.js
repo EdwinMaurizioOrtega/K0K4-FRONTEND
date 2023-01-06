ea91c0fb457cf954fe4a2c9c6baea326110b2d29
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-01-06 02:50:28 -0500

    Ajustes 06ene2023 02h50

--- b2gpl-frontend/src/AppFooter.js
+++ b2gpl-frontend/src/AppFooter.js
@@ -5,7 +5,7 @@ const AppFooter = (props) => {
         <div className="layout-footer">
             <div className="footer-logo-container">
                 <img id="footer-logo" src={`assets/layout/images/logo-${props.colorScheme === 'light' ? 'dark' : 'light'}.png`} alt="atlantis-layout" />
-                <span className="app-name">1KNDY</span>
+                <span className="app-name">K4NDY</span>
             </div>
             <span className="copyright">&#169; B2|GPL - 2023</span>
         </div>
