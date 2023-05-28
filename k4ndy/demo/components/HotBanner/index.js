86ab282936bac7e66700a331257ab75f9db1e662
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-05-28 18:01:22 -0500

    Ajuste texto hot banner

--- k4ndy/demo/components/HotBanner/index.js
+++ k4ndy/demo/components/HotBanner/index.js
@@ -6,7 +6,7 @@ const HotBanner = () =>{
 
         <div id="hot-banner" className="bg-primary text-gray-100 flex justify-content-center align-items-center flex-wrap" style={{textAlign: "center"}}>
             <a className="flex-wrap align-items-center">
-                <span className="underline font-bold">🔥K4NDY es sólo una plataforma de encuentros seguros, no es una agencia de escorts.</span>
+                <span className="underline font-bold">🔥K4NDY es una plataforma de citas seguras, no es una agencia de escorts.</span>
             </a>
             <a className="flex align-items-center no-underline justify-content-center border-circle text-100 hover:bg-bluegray-700 cursor-pointer transition-colors transition-duration-150" style={{ width: '2rem', height: '2rem' }}>
                 <i className="pi pi-times"></i>
